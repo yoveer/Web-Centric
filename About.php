c9517ec688daaa97f8ba80af754e8399468017a1
jeremie1112 <jeremie.daniel@hotmail.com>
2020-02-12 14:40:51 +0400

    worked on these php pages

--- About.php
+++ About.php
@@ -60,8 +60,11 @@
       </li>
     </ul>
   </section>
-  <h5 style="color: grey"><center>We don't have all the above, but it looks classy!!!</center></h5>
-  <div style="padding-bottom: 113px"></div>
+  <section>
+    <h5 style="color: green"><center>We are situated in rose-belle</center></h5>
+    <p style="margin:0;"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d934.8849925853411!2d57.60114482917943!3d-20.401848999146324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjDCsDI0JzA2LjciUyA1N8KwMzYnMDYuMSJF!5e0!3m2!1sen!2smu!4v1581502863198!5m2!1sen!2smu" width="1350" height="600" frameborder="0" style="border:0;" allowfullscreen=""></iframe></p>
+  </section>
+  
   <?php include 'components/footer.php'; ?>
 
 </body>
