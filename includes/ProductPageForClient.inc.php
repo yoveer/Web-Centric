75393d36d7e9289b1fe1644e4bfafe3013dc14ce
jeremie1112 <jeremie.daniel@hotmail.com>
2020-02-16 16:34:14 +0400

    hello

--- includes/ProductPageForClient.inc.php
+++ includes/ProductPageForClient.inc.php
@@ -1,3 +1,49 @@
+<style>
+    .popover
+    {
+        width:100%;
+        max-width: 800px;
+    }
+</style>
+
+<div class="container">
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Menu</span>
+                    <span class="glyphicon glyphicon-menu-hamburger"></span>
+                </button>
+                <a class="navbar-brand" href="/">YOYO</a>
+            </div>
+
+            <div id="navbar-cart" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a id="cart-popover" class="btn" data-placement="bottom" title="Shopping Cart">
+                            <span class="glyphicon glyphicon-shopping-cart"></span>
+                            <span class="badge"></span>
+                            <span class="total_price">Rs 0.00</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>  
+    <div id="popover_content_wrapper" style="display:none;">
+        <span id="cart_details"></span>
+        <div align="right">
+            <a href="#" class="btn btn-primary" id="check_out_cart">
+                <span class="glyphicon glyphicon-shopping-cart"></span> Check out
+            </a>
+            <a href="#" class="btn btn-default" id="clear_cart">
+                <span class="glyphicon glyphicon-trash"></span> Clear
+            </a>
+        </div>
+    </div>
+</div>
+
 <div class="container">
     <div class="row">
         <div class="col">
@@ -24,7 +70,12 @@
                                     <p class="btn btn-danger btn-block">Rs <?php echo $price ?></p>
                                 </div>
                                 <div class="col">
-                                    <a href="#" class="btn btn-success btn-block">Add to cart</a>
+                                    <input type="text" name="quantity" id="quantity<?php echo $ID?>" class="form-control" value="1" />
+                                </div>
+                                <div class="col">
+                                    <input type="hidden" name="hidden_name" id="name<?php echo $ID?>" value="<?php echo $name?>" />
+                                    <input type="hidden" name="hidden_price" id="price<?php echo $ID?>" value="<?php echo $price?>" />
+                                    <input type="button" name="add_to_cart" id="<?php echo $ID?>" style="margin-top:5px;" class="btn btn-success form-control add_to_cart" value="Add to Cart" />
                                 </div>
                             </div>
                             <div class="row">
@@ -59,6 +110,11 @@
         </div>
     </div>
 </div>
+
+
+
+
+
 
 <script>
 $(document).ready(function(){
